ddfcb0a718a9a2a4f25b82daafdfb5a8bc09201f
caoh <test@test.c>
2024-07-08 17:41:55 +0800

    ✨ feat(Register): 添加返回登录页箭头

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,7 +2,16 @@ export default function Register() {
 
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-[#f9c2ec] to-[#abc2ee] flex justify-center items-center">
-            <div className="w-4/12 h-4/5 bg-white rounded-3xl px-20">
+
+            <div className="w-4/12 h-4/5 bg-white rounded-3xl px-20 relative">
+                <div className="flex justify-start">
+                    <a href="login">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 absolute top-5 left-5">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                        </svg>
+                    </a>
+                </div>
+
                 <h1 className="text-6xl font-bold my-12 text-center">Register</h1>
 
                 <div className="w-full py-4 flex flex-col gap-10">
