dfd9ad13cb6ca24cdc947757cf8d0057efed3fdb
caoh <test@test.c>
2024-07-08 17:32:01 +0800

    ✨ feat(Register): 完成注册页基本样式

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Navigate, useRoutes } from 'react-router-dom';
 
+// 登录页
 const Login = React.lazy(() => import('../pages/login'));
+// 注册页
+const Register = React.lazy(() => import('../pages/register'));
 
 const routes = [
     { path: '/', element: <Navigate to="/login" /> },
     {
         path: '/login',
         element: <Login />
+    },
+    {
+        path: '/register',
+        element: <Register />
     }
 ];
 
