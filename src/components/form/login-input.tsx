e2db487f3c39bcaccf507247d09cb10584290bb7
caoh <test@test.c>
2024-07-09 10:05:34 +0800

    ✨ feat(Components、Login): 完成封装LoginInput组件

---
+++ src/components/form/login-input.tsx
@@ -0,0 +1,23 @@
+
+interface LoginInputProps {
+    label: string;
+    value: string;
+    type: string;
+}
+export default function LoginInput(props: LoginInputProps) {
+    return (
+        <div className="relative">
+            <input type={props.type} id={props.label} value={props.value}
+                className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-indigo-500" placeholder='' required
+            />
+            <label htmlFor={props.label} className="
+                absolute left-0 top-2 transition-all ease-in
+                peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400
+                peer-focus:-top-5 peer-focus:text-sm peer-focus:text-indigo-600
+                peer-valid:-top-5 peer-valid:text-sm peer-valid:text-indigo-600
+            ">
+                {props.label}
+            </label>
+        </div>
+    )
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,33 +1,21 @@
+import { useState } from "react";
+import LoginInput from "../components/form/login-input";
+
 export default function Login() {
+    const [formValue, setFormValue] = useState({
+        username: '',
+        password: ''
+    });
+
     return (
         <div className="w-screen h-screen bg-gradient-to-r from-[#f9c2ec] to-[#abc2ee] flex justify-center items-center">
             <div className="w-4/12 h-4/5 bg-white rounded-3xl px-20">
                 <h1 className="text-6xl font-bold my-32 text-center">Login</h1>
 
                 <div className="w-full py-4 flex flex-col gap-10">
-                    <div className="relative">
-                        <input type="text" id="Username" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-indigo-500" placeholder=" " required />
-                        <label htmlFor="Username" className="
-                            absolute left-0 top-2 transition-all ease-in
-                            peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400
-                            peer-focus:-top-5 peer-focus:text-sm peer-focus:text-indigo-600
-                            peer-valid:-top-5 peer-valid:text-sm peer-valid:text-indigo-600
-                        ">
-                            Username
-                        </label>
-                    </div>
+                    <LoginInput type="text" label="Username" value={formValue.username}/>
 
-                    <div className="relative">
-                        <input type="password" id="Password" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 placeholder-transparent focus:outline-none focus:border-indigo-500" placeholder=" " required />
-                        <label htmlFor="Password" className="
-                            absolute left-0 top-2 transition-all ease-in
-                            peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 
-                            peer-focus:-top-5 peer-focus:text-sm peer-focus:text-indigo-600
-                            peer-valid:-top-5 peer-valid:text-sm peer-valid:text-indigo-600
-                        ">
-                            Password
-                        </label>
-                    </div>
+                    <LoginInput type="password" label="Password" value={formValue.password}/>
 
                     <button className="w-full h-10 bg-indigo-500 text-white rounded-sm hover:bg-indigo-600">Log in</button>
                 </div>
