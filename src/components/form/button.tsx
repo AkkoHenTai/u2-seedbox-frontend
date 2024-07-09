e61a07cb8e918bf04ab093d21b48d8dd4f56dc0c
caoh <test@test.c>
2024-07-09 16:05:04 +0800

    ✨ feat(Button): 完成禁用样式

--- src/components/form/button.tsx
+++ src/components/form/button.tsx
@@ -3,6 +3,7 @@ interface ButtonProps {
     children: React.ReactNode;
     disabled?: boolean;
     loading?: boolean;
+    onClick: () => void;
 }
 
 // 按钮样式
@@ -13,12 +14,15 @@ const styles = {
     warning: 'w-full h-10 bg-yellow-500 text-white rounded-sm hover:bg-yellow-600 active:bg-yellow-700 shadow-md hover:shadow-lg active:shadow-sm transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 border border-transparent hover:border-yellow-400',
     info: 'w-full h-10 bg-blue-500 text-white rounded-sm hover:bg-blue-600 active:bg-blue-700 shadow-md hover:shadow-lg active:shadow-sm transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 border border-transparent hover:border-blue-400',
 }
+// 按钮禁用样式
+const disabledStyles = 'bg-gray-300 text-gray-500 cursor-not-allowed hover:bg-gray-300 active:bg-gray-300 shadow-md hover:shadow-lg active:shadow-sm transition-all duration-300 ease-in-out transform hover:scale-105 active:scale-95 border border-transparent hover:border-gray-300';
 
-export default function Button({ disabled = false, loading = false, ...props }: ButtonProps) {
+export default function Button(props: ButtonProps) {
     return (
         <button
-            className={`${styles[props.type]} ${disabled ? 'btn-disabled' : ''}`}
-            disabled={disabled}
+            className={`${styles[props.type]} ${props.disabled ? disabledStyles : ''}`}
+            disabled={props.disabled}
+            onClick={props.onClick}
         >
             {props.children}
         </button>
